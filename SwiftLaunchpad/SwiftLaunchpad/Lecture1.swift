9c0247c63bdd43bd6c69cb8a5de56dfef8b00cd3
Vaibhav Singla <vaibhavsingla1996@gmail.com>
2019-09-28 20:01:49 +0530

    some setup completed 1 question of lecture 1

---
+++ SwiftLaunchpad/SwiftLaunchpad/Lecture1.swift
@@ -0,0 +1,18 @@
+//
+//  Lecture1.swift
+//  SwiftLaunchpad
+//
+//  Created by Vaibhav Singla on 28/09/19.
+//  Copyright © 2019 Vaibhav Singla. All rights reserved.
+//
+
+import Foundation
+
+class Lecture1 {
+    static func checkOddEven(input: Int) -> Bool {
+        if input%2 == 0 {
+            return true
+        }
+        return false
+    }
+}
---
+++ SwiftLaunchpad/SwiftLaunchpad/main.swift
@@ -0,0 +1,15 @@
+//
+//  main.swift
+//  SwiftLaunchpad
+//
+//  Created by Vaibhav Singla on 28/09/19.
+//  Copyright © 2019 Vaibhav Singla. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+print("hey there")
+//  Lecture 1
+let input = Int(readLine())
+Lecture1.checkOddEven(input: input)
